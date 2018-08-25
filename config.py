c1427762640e6e236a820c1b4226a54bf3cf5c93
Fred Abood <fred@fredabood.com>
2018-08-25 13:01:13 -0500

    adding .gitignore

--- config.py
+++
@@ -1,7 +0,0 @@
-creds = {
-    'client_id':'Qq-JzuVpSoll6A',
-    'client_secret':'TLuB22KOKv94asMTMzvTmP1nj8c',
-    'user_agent':'fred.one',
-    'username':'fredabood',
-    'password':'nQ2hx,WvoZPmUdEN3^*o6KuZ/d4882T4o&spYw,X+7%8pE=Ea/'
-}
